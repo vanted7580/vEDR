56a550685e44c931b5f0b3fe010e059cfa832066
vanted7580 <wqy224491@gmail.com>
2025-07-12 15:23:13 +0800

    Test commit

--- SCLoader/xor.h
+++ SCLoader/xor.h
@@ -1,5 +1,4 @@
 #pragma once
 #include <cstdint>
 
-void xor_cipher(unsigned char* data, std::size_t length, std::uint8_t key);
-//void print_hex_formatted(const unsigned char* data, std::size_t len);
+void xor_cipher(unsigned char *data, std::size_t length, std::uint8_t key);
--- vEDR_DLL/main.cpp
+++ vEDR_DLL/main.cpp
@@ -1,11 +1,8 @@
-
 #include <windows.h>
 #include <iostream>
 
-BOOL APIENTRY DllMain(HANDLE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
-{
-    switch (ul_reason_for_call)
-    {
+BOOL APIENTRY DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
         case DLL_PROCESS_ATTACH:
             std::cout << "Dll attached!" << std::endl;
             break;
